8ec4207b1354cddbe6bfed081d646c39833f5f46
displaycoffee <adria.m.murphy@gmail.com>
2026-06-11 20:13:34 -0600

    updates

--- src/_config/scripts/utils.ts
+++ src/_config/scripts/utils.ts
@@ -1,7 +1,7 @@
 export const utils: UtilsType = {
 	getLast: (value: string | string[], delimeter?: string) => {
 		// Get last item in array
-		let valueArray = [] as string[] | number[];
+		let valueArray: string[] | number[] = [];
 		if (Array.isArray(value)) {
 			valueArray = value;
 		} else if (delimeter) {
